0fa32315724b77678ba3a80ff429da4d8923c4d8
Chaitanya Mohan <52050123+Chaitanyamohan9999@users.noreply.github.com>
2020-09-27 12:09:30 -0400

    initial commit

---
+++ app/src/main/java/com/food/foodzone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.food.foodzone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
