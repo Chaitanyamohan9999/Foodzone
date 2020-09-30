63bd61a367dfc2e47f54f2717f5d783bfdcd510e
Chaitanya Mohan <52050123+Chaitanyamohan9999@users.noreply.github.com>
2020-09-30 01:15:28 -0400

    splash activity  committed

---
+++ app/src/main/java/com/food/foodzone/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.food.foodzone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/food/foodzone/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.food.foodzone.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.food.foodzone.R;
+
+
+public class SplashActivity extends com.food.foodzone.activities.BaseActivity {
+
+    private LinearLayout llSplash;
+
+    @Override
+    public void initialise() {
+        llSplash = (LinearLayout) inflater.inflate(R.layout.activity_splash, null);
+        addBodyView(llSplash);
+        lockMenu();
+        ivBack.setVisibility(View.GONE);
+        llToolbar.setVisibility(View.GONE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, UserTypeActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+                finish();
+            }
+        }, 500);
+    }
+
+    @Override
+    public void getData() {
+
+    }
+}
