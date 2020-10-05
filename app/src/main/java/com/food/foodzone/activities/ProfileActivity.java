106c60f567dfa6e2de638f944d14d76404d48e1e
Raghu5522 <raghu.naluvala@yahoo.com>
2020-10-05 19:05:34 -0400

    updated profileActivity

--- app/src/main/java/com/food/foodzone/activities/ProfileActivity.java
+++ app/src/main/java/com/food/foodzone/activities/ProfileActivity.java
@@ -59,6 +59,61 @@ public class ProfileActivity extends BaseActivity {
                 finish();
             }
         });
+        getData();
+        ivEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                civProfile.setAlpha(1f);
+                civProfile.setEnabled(true);
+                etName.setEnabled(true);
+                etEmail.setEnabled(true);
+                etContactNumber.setEnabled(true);
+                etCity.setEnabled(true);
+                etState.setEnabled(true);
+                etCountry.setEnabled(true);
+                etName.setBackgroundResource(R.drawable.edit_text_bg);
+                etEmail.setBackgroundResource(R.drawable.edit_text_bg);
+                etContactNumber.setBackgroundResource(R.drawable.edit_text_bg);
+                etCity.setBackgroundResource(R.drawable.edit_text_bg);
+                etState.setBackgroundResource(R.drawable.edit_text_bg);
+                etCountry.setBackgroundResource(R.drawable.edit_text_bg);
+                btnUpdate.setVisibility(View.VISIBLE);
+            }
+        });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etName.getText().toString().equalsIgnoreCase("")){
+                    showErrorMessage("Please enter name");
+                }
+                else if(etEmail.getText().toString().equalsIgnoreCase("")){
+                    showErrorMessage("Please enter email");
+                }
+                else if(!isValidEmail(etEmail.getText().toString().trim())){
+                    showErrorMessage("Please enter valid email");
+                }
+                else if(etContactNumber.getText().toString().trim().equalsIgnoreCase("")){
+                    showErrorMessage("Please enter contact number");
+                }
+                else if(etCity.getText().toString().trim().equalsIgnoreCase("")){
+                    showErrorMessage("Please enter city");
+                }
+                else if(etState.getText().toString().trim().equalsIgnoreCase("")){
+                    showErrorMessage("Please enter province");
+                }
+                else if(etCountry.getText().toString().trim().equalsIgnoreCase("")){
+                    showErrorMessage("Please enter country");
+                }
+                else {
+                    userDo.name   = etName.getText().toString().trim();
+                    userDo.phone  = etContactNumber.getText().toString().trim();
+                    userDo.city   = etCity.getText().toString().trim();
+                    userDo.state  = etState.getText().toString().trim();
+                    userDo.country  = etCountry.getText().toString().trim();
+                    updateProfile(userDo);
+                }
+            }
+        });
         civProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -102,5 +157,64 @@ public class ProfileActivity extends BaseActivity {
                 }
             }
         }
+    }
+
+    private void selectProfilePic(int requestCode){
+        String userId = preferenceUtils.getStringFromPreference(PreferenceUtils.UserId, "");
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String imagePath = "Profile_"+userId+"_" + System.currentTimeMillis() + ".png";
+        File file = new File(Environment.getExternalStorageDirectory(), imagePath);
+        imageUri = Uri.fromFile(file);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        cameraIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        Intent chooser = Intent.createChooser(galleryIntent, "Select an option");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] { cameraIntent });
+        startActivityForResult(chooser, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 110 && resultCode == RESULT_OK){
+            if(data == null){// from camer
+                civProfile.setImageURI(imageUri);
+            }
+            else {// from file storage
+                imageUri = data.getData();
+                civProfile.setImageURI(imageUri);
+            }
+            uploadProfilePic();
+        }
+    }
+
+    private void uploadProfilePic(){
+    }
+
+    private void updateProfile(UserDo userDo){
+    }
+
+    @Override
+    public void getData() {
+    }
+
+    private void bindData(UserDo userDo){
+        try {
+            etName.setText(userDo.name);
+            etEmail.setText(userDo.email);
+            etContactNumber.setText(userDo.phone);
+            etCity.setText(userDo.city);
+            etState.setText(userDo.state);
+            etCountry.setText(userDo.country);
+            if(!userDo.profilePicUrl.equalsIgnoreCase("")){
+                Picasso.get().load(userDo.profilePicUrl).placeholder(R.drawable.user_icon)
+                        .error(R.drawable.user_icon).into(civProfile);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
