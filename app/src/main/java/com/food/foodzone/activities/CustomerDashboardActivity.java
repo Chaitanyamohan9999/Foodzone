c140984d7ca8d6f1fff51606761aad0ce5d2fe6c
yaminireddy <yaminireddy1995@gmail.com>
2020-10-07 11:12:11 -0400

    customer dashboard activity updated

--- app/src/main/java/com/food/foodzone/activities/CustomerDashboardActivity.java
+++ app/src/main/java/com/food/foodzone/activities/CustomerDashboardActivity.java
@@ -8,7 +8,74 @@ import com.food.foodzone.R;
 import com.food.foodzone.common.*;
 import com.food.foodzone.utils.PreferenceUtils;
 
-public class CustomerDashboardActivity {
+public class CustomerDashboardActivity extends BaseActivity {
 
+    private View llDashboard;
+    private TextView tvUserName, tvDineIn, tvTakeOut, tvReservation;
 
+    @Override
+    public void initialise() {
+        llDashboard = inflater.inflate(R.layout.customer_dashboard_layout, null);
+        addBodyView(llDashboard);
+        lockMenu();
+        flCart.setVisibility(View.VISIBLE);
+        ivBack.setVisibility(View.GONE);
+        ivMenu.setVisibility(View.VISIBLE);
+        llToolbar.setVisibility(View.VISIBLE);
+        initialiseControls();
+        String welcomeText = "Hi "+preferenceUtils.getStringFromPreference(PreferenceUtils.UserName, "")+", Welcome to Food Zone";
+        tvUserName.setText(welcomeText);
+        tvDineIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerDashboardActivity.this, TablesListActivity.class);
+                intent.putExtra(AppConstants.From, AppConstants.DiveIn);
+                startActivity(intent);
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+            }
+        });
+        tvTakeOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerDashboardActivity.this, MenuListActivity.class);
+                intent.putExtra(AppConstants.From, AppConstants.TakeOut);
+                startActivity(intent);
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+            }
+        });
+        tvReservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerDashboardActivity.this, TablesListActivity.class);
+                intent.putExtra(AppConstants.From, AppConstants.Reservation);
+                startActivity(intent);
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+            }
+        });
+    }
+
+    private void initialiseControls() {
+        tvUserName           = llDashboard.findViewById(R.id.tvUserName);
+        tvDineIn             = llDashboard.findViewById(R.id.tvDineIn);
+        tvTakeOut            = llDashboard.findViewById(R.id.tvTakeOut);
+        tvReservation        = llDashboard.findViewById(R.id.tvReservation);
+    }
+    @Override
+    public void getData() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        menuOperates();
+        showAppCompatAlert("", "Do you want to exit from app?", "Exit", "Cancel", AppConstants.Exit, false);
+    }
+
+    @Override
+    public void onButtonYesClick(String from) {
+        super.onButtonYesClick(from);
+        if(from.equalsIgnoreCase(AppConstants.Exit)){
+            finish();
+        }
+    }
 }
