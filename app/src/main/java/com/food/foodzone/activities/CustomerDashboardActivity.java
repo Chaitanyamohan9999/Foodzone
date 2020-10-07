37834ac886f878bf3c2722910f0d52efe1a87dbc
yaminireddy <yaminireddy1995@gmail.com>
2020-10-07 10:50:30 -0400

    customer dashboard layout updated

---
+++ app/src/main/java/com/food/foodzone/activities/CustomerDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.food.foodzone.activities;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.food.foodzone.R;
+import com.food.foodzone.common.*;
+import com.food.foodzone.utils.PreferenceUtils;
+
+public class CustomerDashboardActivity {
+
+
+}
