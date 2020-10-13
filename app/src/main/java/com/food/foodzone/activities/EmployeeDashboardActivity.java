8cc12a3de39283ec3e13c719128a972edb098f22
Chaitanya Mohan <52050123+Chaitanyamohan9999@users.noreply.github.com>
2020-10-13 11:59:15 -0400

    employee activity updated

--- app/src/main/java/com/food/foodzone/activities/EmployeeDashboardActivity.java
+++ app/src/main/java/com/food/foodzone/activities/EmployeeDashboardActivity.java
@@ -1,6 +1,7 @@
 package com.food.foodzone.activities;
 
 import android.content.Intent;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,17 +11,17 @@ import com.food.foodzone.common.AppConstants;
 public class EmployeeDashboardActivity extends BaseActivity {
 
     private View llDashboard;
-    private TextView tvDineIn, tvTakeOut, tvReservation;
+    private TextView tvManageEmployees, tvManageTables, tvManageMenu;
 
     @Override
     public void initialise() {
         llDashboard = inflater.inflate(R.layout.employee_dashboard_layout, null);
         addBodyView(llDashboard);
-        lockMenu();
         flCart.setVisibility(View.GONE);
         ivBack.setVisibility(View.GONE);
-        ivMenu.setVisibility(View.GONE);
+        ivMenu.setVisibility(View.VISIBLE);
         llToolbar.setVisibility(View.VISIBLE);
+        employeeLeftMenu();
         initialiseControls();
         tvManageEmployees.setOnClickListener(new View.OnClickListener() {
             @Override
