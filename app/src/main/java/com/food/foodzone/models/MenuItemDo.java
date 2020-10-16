da114776e3b1999c1ebc8aed367b7190fca18301
HARSHU2416 <takkalapallyharshini@gmail.com>
2020-10-16 13:14:39 -0400

    menuitemdo model updated

--- app/src/main/java/com/food/foodzone/models/MenuItemDo.java
+++ app/src/main/java/com/food/foodzone/models/MenuItemDo.java
@@ -3,20 +3,26 @@ package com.food.foodzone.models;
 public class MenuItemDo extends BaseDo {
 
     public String itemId = "";
+    public String itemCategory = "";
     public String itemName = "";
     public double itemPrice;
     public String itemDescription = "";
-    public String itemImage;
+    public String itemImage = "";
     public boolean isAvailable;
+    public int quantity;
+    public boolean isCategory;
 
     public MenuItemDo() {}
 
-    public MenuItemDo(String itemId, String itemName, double itemPrice, String itemDescription, String itemImage, boolean isAvailable) {
+    public MenuItemDo(String itemId, String itemCategory, String itemName, double itemPrice, String itemDescription, String itemImage, boolean isAvailable, int quantity) {
         this.itemId = itemId;
+        this.itemCategory = itemCategory;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
         this.itemDescription = itemDescription;
         this.itemImage = itemImage;
         this.isAvailable = isAvailable;
+        this.quantity = quantity;
     }
+
 }
