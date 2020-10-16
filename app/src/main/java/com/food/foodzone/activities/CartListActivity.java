528df9b66896f3abc87dcd0e60d98157d411b71b
Chaitanya Mohan <52050123+Chaitanyamohan9999@users.noreply.github.com>
2020-10-15 23:19:02 -0400

    cart list commited

---
+++ app/src/main/java/com/food/foodzone/activities/CartListActivity.java
@@ -0,0 +1,29 @@
+package com.food.foodzone.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.food.foodzone.R;
+import com.food.foodzone.common.AppConstants;
+import com.food.foodzone.models.MenuItemDo;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+
+public class CartListActivity extends BaseActivity {
+
+   
+}
