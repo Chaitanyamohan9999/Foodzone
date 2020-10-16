8449acbc14509f3898334b860ca1391ce1b08bc2
Chaitanya Mohan <52050123+Chaitanyamohan9999@users.noreply.github.com>
2020-10-16 13:11:09 -0400

    Tables do updated

--- app/src/main/java/com/food/foodzone/models/TableDo.java
+++ app/src/main/java/com/food/foodzone/models/TableDo.java
@@ -3,16 +3,18 @@ package com.food.foodzone.models;
 public class TableDo extends BaseDo {
 
     public String tableId = "";
-    public String tableName = "";
+    public int tableNumber;
+    public String tableType = "";
     public String reservedBy = "";
     public String reservedAt = "";
     public int tableCapacity;
 
     public TableDo() {}
 
-    public TableDo(String tableId, String tableName, int tableCapacity, String reservedBy, String reservedAt) {
+    public TableDo(String tableId, int tableNumber, String tableType, int tableCapacity, String reservedBy, String reservedAt) {
         this.tableId = tableId;
-        this.tableName = tableName;
+        this.tableNumber = tableNumber;
+        this.tableType = tableType;
         this.tableCapacity = tableCapacity;
         this.reservedBy = reservedBy;
         this.reservedAt = reservedAt;
