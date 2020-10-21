a4bc90a7cdbf83446548a255205d0604030b5afe
Chaitanya Mohan <52050123+Chaitanyamohan9999@users.noreply.github.com>
2020-10-20 23:47:07 -0400

    schedule time actitvity and layout  updated

---
+++ app/src/main/java/com/food/foodzone/activities/ScheduleTimeActivity.java
@@ -0,0 +1,35 @@
+package com.food.foodzone.activities;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.food.foodzone.R;
+import com.food.foodzone.common.AppConstants;
+import com.food.foodzone.models.TableDo;
+import com.food.foodzone.utils.CustomDateTimePicker;
+import com.food.foodzone.utils.RangeTimePickerDialog;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ScheduleTimeActivity extends BaseActivity {
+
+
+
+
+}
