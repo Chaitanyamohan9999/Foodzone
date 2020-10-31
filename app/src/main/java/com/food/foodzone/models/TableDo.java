3f46555d0d0f4d145eb6400900d1551ed9d61f3e
VinayReddy <vinayreddy6668@gmail.com>
2020-10-31 18:57:10 -0400

    TableDo updated

--- app/src/main/java/com/food/foodzone/models/TableDo.java
+++ app/src/main/java/com/food/foodzone/models/TableDo.java
@@ -7,18 +7,20 @@ public class TableDo extends BaseDo {
     public String tableType = "";
     public String reservedBy = "";
     public long reservedAt;
+    public int reservedFor;
     public int tableCapacity;
     public String tableStatus = "";
 
     public TableDo() {}
 
-    public TableDo(String tableId, int tableNumber, String tableType, int tableCapacity, String reservedBy, long reservedAt, String tableStatus) {
+    public TableDo(String tableId, int tableNumber, String tableType, int tableCapacity, String reservedBy, long reservedAt, int reservedFor, String tableStatus) {
         this.tableId = tableId;
         this.tableNumber = tableNumber;
         this.tableType = tableType;
         this.tableCapacity = tableCapacity;
         this.reservedBy = reservedBy;
         this.reservedAt = reservedAt;
+        this.reservedFor = reservedFor;
         this.tableStatus = tableStatus;
     }
 }
